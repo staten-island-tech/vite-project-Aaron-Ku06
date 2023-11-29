3e737ea955b50587c32eb845f738f2b47c27a5f7
Aaron-Ku06 <aaronku320@gmail.com>
2023-11-29 00:30:06 -0500

    Finished

--- vite-project/js/Menu.js
+++ vite-project/js/Menu.js
@@ -9,7 +9,7 @@ const cards = [{
     DomainName: "N/A",
     Status: "Dead",
     Living: false,
-    Image: "",
+    Image: "https://pm1.aminoapps.com/7835/1315d8aeb5c9b364b4332f284b5c959960fc800br1-724-990v2_hq.jpg",
 },
 {
     Name: "Satoru Gojo",
@@ -18,7 +18,7 @@ const cards = [{
     DomainName: "Infinite Void",
     Status: "Alive",
     Living: true,
-    Image: "",
+    Image: "https://pm1.aminoapps.com/7701/915118c4011d8199ea6c2ee0e49331b0f9123985r1-360-450v2_00.jpg",
 },
 {
     Name: "Megumi Fushiguro",
@@ -27,7 +27,7 @@ const cards = [{
     DomainName: "Chimera Shadow Garden ",
     Status: "Alive",
     Living: true,
-    Image: "",
+    Image: "https://pm1.aminoapps.com/7619/908bdd5f412f6cf2306c888ee2e5a00638f9a84dr1-1609-2048v2_00.jpg",
 },
 {
     Name: "Toge Inumaki",
@@ -36,7 +36,7 @@ const cards = [{
     DomainName: "N/A",
     Status: "Alive",
     Living: true,
-    Image: "",
+    Image: "https://pm1.aminoapps.com/8172/e63d18afcee0724200861867a884952d2e791dd9r1-750-1164v2_hq.jpg",
 },
 {
     Name: "Yuta Okkotsu",
@@ -45,7 +45,7 @@ const cards = [{
     DomainName: "Penis Expansion Surgery",
     Status: "Alive",
     Living: true,
-    Image: "https://static.wikia.nocookie.net/jujutsu-kaisen/images/7/79/Yuta_Okkotsu_%28Chapter_137%29.png/revision/latest?cb=20210201190010",
+    Image: "https://pm1.aminoapps.com/8342/d6ac1e4c7321f6b330e1926dc3c8b2ab8d660172r1-1080-1589v2_hq.jpg",
 },
 {
     Name: "Maki Zenin",
@@ -54,7 +54,7 @@ const cards = [{
     DomainName: "N/A",
     Status: "Alive",
     Living: true,
-    Image: "",
+    Image: "https://i.pinimg.com/564x/65/d5/79/65d5793e50f80ec856236f0be5c5bfe7.jpg",
 },
 {
     Name: "Kento Nanami",
@@ -63,7 +63,7 @@ const cards = [{
     DomainName: "N/A",
     Status: "Dead",
     Living: false,
-    Image: "",
+    Image: "https://pm1.aminoapps.com/7845/2e13f839f65633f3066d6020e41d8824aa133251r1-700-985v2_00.jpg",
 },
 ];
 
@@ -117,7 +117,7 @@ document.querySelector(".domain").addEventListener("click", function () {
         "afterbegin",
         `<div class="card" id="card">
         <h2 class="Sorcerers">${domainTrueCard.Name}</h2>
-        <img class="image" src="${domainTrueCard.image}" />
+        <img class="image" src="${domainTrueCard.Image}" />
         <h3 class="descriptions" id="domain">Domain: ${domainTrueCard.DomainName}</h3>
         <h3 class="descriptions" id="clan">${domainTrueCard.Clan}</h3>
         <h3 class="descriptions" id="status">${domainTrueCard.Status}</h3>
@@ -133,7 +133,7 @@ document.querySelector(".status").addEventListener("click", function () {
         "afterbegin",
         `<div class="card" id="card">
         <h2 class="Sorcerers">${statusTrueCard.Name}</h2>
-        <img class="image" src="${statusTrueCard.image}" />
+        <img class="image" src="${statusTrueCard.Image}" />
         <h3 class="descriptions" id-"domain">Domain: ${statusTrueCard.DomainName}</h3>
         <h3 class="descriptions" id="clan">${statusTrueCard.Clan}</h3>
         <h3 class="descriptions" id="status>${statusTrueCard.Status}</h3>
@@ -149,7 +149,7 @@ document.querySelector(".family").addEventListener("click", function () {
       "afterbegin",
       `<div class="card" id="card">
       <h2 class="Sorcerers">${zeninCard.Name}</h2>
-      <img class="image" src="${zeninCard.image}"</img>
+      <img class="image" src="${zeninCard.Image}"</img>
       <h3 class="descriptions" id="domain">Domain: ${zeninCard.DomainName}</h3>
       <h3 class="descriptions" id="clan">${zeninCard.Clan}</h3>
       <h3 class="descriptions" id="status">${zeninCard.Status}</h3>
